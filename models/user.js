518f0ca0cf3198cec4fec10e443eb21991a80d26
soumyarajbag <soumyarajbag@gmail.com>
2023-10-15 03:16:56 +0530

    HomePage done

---
+++ models/user.js
@@ -0,0 +1,23 @@
+import { Schema, model, models } from "mongoose";
+
+const UserScema = new Schema({
+  email: {
+    type: String,
+    required: [true, "Email is required !"],
+    unique: [true, "Email already exists"],
+  },
+  username: {
+    type: String,
+    required: [true, "Username is required!"],
+    match: [
+      /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
+      "Username invalid, it should contain 8-20 alphanumeric letters and be unique!",
+    ],
+  },
+  image: {
+    type: String,
+  },
+});
+
+const User = models.User || model("User", UserScema);
+export default User;
